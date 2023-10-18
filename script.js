a98c05bfb716b843365b32a0f7a8297f5aad347a
TomE <tommevans210@gmail.com>
2023-10-18 15:57:02 +0700

    first

---
+++ script.js
@@ -0,0 +1,25 @@
+// script for tab links
+
+var tabLinks = document.getElementsByClassName('tab-links')
+var tabContents = document.getElementsByClassName('tab-contents')
+
+function opentab(tabname) {
+  for (tabLink of tabLinks) {
+    tabLink.classList.remove('active-link')
+  }
+  for (tabContent of tabContents) {
+    tabContent.classList.remove('active-tab')
+  }
+  event.currentTarget.classList.add('active-link')
+  document.getElementById(tabname).classList.add('active-tab')
+}
+
+// script for menu
+var sidemenu = document.getElementById('sidemenu')
+
+function openmenu() {
+  sidemenu.style.right = '0'
+}
+function closemenu() {
+  sidemenu.style.right = '-200px'
+}
